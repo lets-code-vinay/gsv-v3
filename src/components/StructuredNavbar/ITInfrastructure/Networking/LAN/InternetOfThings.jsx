5190945e95ef3c8ebc0ffa5be1dbc9d1a70e86d2
vinay <vinay.shermauryan@gmail.com>
2024-02-18 20:18:12 +0530

    refactor code

--- src/components/StructuredNavbar/ITInfrastructure/Networking/LAN/InternetOfThings.jsx
+++ src/components/StructuredNavbar/ITInfrastructure/Networking/LAN/InternetOfThings.jsx
@@ -1,32 +1,40 @@
-import { Box } from '@mui/material'
-import React, { useState } from 'react'
-import ImageText from '../../../../More/SideBarTab/ImageText'
-import TextImage from '../../../../OrdinaryComponents/TextImage'
-import { IOT_Data } from '../../../../../configs/ITInfrastructure/Networking/LAN/IOT'
-import ShowData from '../../../../OrdinaryComponents'
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import { IOT_Data } from "../../../../../configs/ITInfrastructure/Networking/LAN/IOT";
+import ShowData from "../../../../OrdinaryComponents";
 
 const InternetOfThings = () => {
-    const [data, setData] = useState(IOT_Data)
+  const [data, setData] = useState(IOT_Data);
 
-    return <>
-        <Box sx={{ background: '#44a8f44f', borderRadius: '20px', fontWeight: '900', margin: '8% 5% 0% 5%', fontSize: '50px', display: 'flex', justifyContent: 'center' }}>
-
-            {data?.title}
-        </Box>
-        {
-            data?.child?.map((text, index) => {
-                return <>
-                    <ShowData
-                        title={data?.title}
-                        details={text.details}
-                        image={text.image}
-                        alt='img'
-                    />
-                </>
-            })
-        }
-
+  return (
+    <>
+      <Box
+        sx={{
+          background: "#44a8f44f",
+          borderRadius: "20px",
+          fontWeight: "900",
+          margin: "8% 5% 0% 5%",
+          fontSize: "50px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {data?.title}
+      </Box>
+      {data?.child?.map((text, index) => {
+        return (
+          <>
+            <ShowData
+              title={data?.title}
+              details={text.details}
+              image={text.image}
+              alt="img"
+            />
+          </>
+        );
+      })}
     </>
-}
+  );
+};
 
-export default InternetOfThings
+export default InternetOfThings;
