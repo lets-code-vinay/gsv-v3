68968d680deeed78f99a686735f20cacf2cfe9d4
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-02-18 11:19:46 +0530

    Added assurance insight page

---
+++ src/components/StructuredNavbar/ITInfrastructure/Data Center/DataCenter/Assurence_Insight.jsx
@@ -0,0 +1,28 @@
+import { Box } from '@mui/material'
+import React, { useState } from 'react'
+import ShowData from '../../../../OrdinaryComponents'
+import { ManagedDetection_Data } from '../../../../../configs/ITInfrastructure/Security/CyberSecurity/ManagedDetectionResponse'
+const Manage_Detection_Res = () => {
+    const [data, setData] = useState(ManagedDetection_Data)
+
+    return <>
+        <Box sx={{ background: '#44a8f44f', borderRadius: '20px', fontWeight: '900', margin: '8% 5% 0% 5%', fontSize: '50px', display: 'flex', justifyContent: 'center' }}>
+
+            {data?.title}
+        </Box>
+        {
+            data?.child?.map((text, index) => {
+                return <>
+                    <ShowData
+                        title={data?.title}
+                        details={text.details}
+                        image={text.image}
+                        alt='img'
+                    />
+                </>
+            })
+        }
+    </>
+}
+
+export default Manage_Detection_Res
